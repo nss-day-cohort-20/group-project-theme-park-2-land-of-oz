c274a02e11401ee9938b96d1ee75b1cf1a584a29
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-07-12 15:50:27 -0500

    saving my ass

--- javascripts/search.js
+++ javascripts/search.js
@@ -24,28 +24,29 @@ let searchMatcher = function() {
       console.log("successes", matchSuccesses);
     }
   });
+  return matchSuccesses;
 };
 
 
-// let timeMatcher = function(){
-//   matchSuccesses = [];
-//   $.ajax({
-//     url: "https://fir-101-143cd.firebaseio.com/attractions.json",
-//     success: function(data){
-//       var searchText = document.getElementById("timeSubmission").value;
-//       var reg = new RegExp(`${searchText}`, "i");
-//       objArray.push(data);
-//       console.log(reg);
-//       objArray[0].forEach( function (attraction){
-//         if (attraction.name.search(reg) === -1){
-//         }
-//         else{
-//           matchSuccesses.push(attraction);
-//         }
-//       });
-//       console.log("successes", matchSuccesses);
-//     }
-//   });
-// }
-//
+let timeMatcher = function(){
+  matchSuccesses = [];
+  $.ajax({
+    url: "https://fir-101-143cd.firebaseio.com/attractions.json",
+    success: function(data){
+      var searchText = document.getElementById("timeSubmission").value;
+      var reg = new RegExp(`${searchText}`, "i");
+      objArray.push(data);
+      console.log(reg);
+      objArray[0].forEach( function (attraction){
+        if (attraction.name.search(reg) === -1){
+        }
+        else{
+          matchSuccesses.push(attraction)
+        }
+      });
+      console.log("successes", matchSuccesses);
+    }
+  });
+};
+
 module.exports = {searchMatcher};
