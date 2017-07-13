02c4a99765f02a394f146aabde7781af82d89ece
Sam <samb283@gmail.com>
2017-07-13 11:21:13 -0500

    updated templateBuilder

--- javascripts/templateBuilder.js
+++ javascripts/templateBuilder.js
@@ -2,13 +2,9 @@
 
 let $ = require('jquery');
 let clickTemplate = require('../templates/clickTemplate.hbs');
-let searchTemplate = require('../templates/searchTemplate.hbs');
-let timeTemplate = require('../templates/timeTemplate.hbs');
 
-function makeInfoPane(matchedAttractions, whichEvent){
-    if (whichEvent==="click") {return clickTemplate({attractions: matchedAttractions});}
-    if (whichEvent==="search") {return searchTemplate({attractions: matchedAttractions});}
-    if (whichEvent==="time") {return timeTemplate({attractions: matchedAttractions});}
+function makeInfoPane(matchedAttractions){
+    return clickTemplate({attractions: matchedAttractions});
 }
 
 module.exports = {makeInfoPane};
