9d649dfcf93ee2d6d0d6430cb117e24c3093d778
Chris Mellor <chrismellor@outlook.com>
2017-07-14 12:12:44 -0500

    now it should work

--- javascripts/infoPaneController.js
+++ javascripts/infoPaneController.js
@@ -27,26 +27,28 @@ function loadInfoByTime(){
         });
     });
 }
-console.log("who wants this dog???");
+console.log("who wants this dog?");
 function loadInfoByClick(whichArea) {
-
     accordianFactory.getAttractions()
-    .then((attractions)=>{
-        let searchResultAttractions = search.areaMatcher(attractions, whichArea);
-        accordianFactory.getAttractionTypes()
-        .then((attractionTypes)=>{
-            searchResultAttractions.forEach((attraction)=>{
-                attractionTypes.forEach((type)=>{
-                    // console.log("attractiontype", attraction.type_id);
-                    if(type.id === attraction.type_id){
-                        attraction.type = type.name;
-                    }
+        .then((attractions) => {
+            let searchResultAttractions = search.areaMatcher(attractions, whichArea);
+            accordianFactory.getAttractionTypes()
+                .then((attractionTypes) => {
+                    searchResultAttractions.forEach((attraction) => {
+                        attractionTypes.forEach((type) => {
+                            if (type.id === attraction.type_id) {
+                                attraction.type = type.name;
+                            }
+                            searchResultAttractions.forEach((attraction) => {
+                                if (attraction.times === undefined) {
+                                    attraction.times = {"operatingHours" : `Open 9:00 AM to 10:00 PM`};
+                                }
+                            });
+                        });
+                        let infoForPain = builder.makeInfoPane(searchResultAttractions);
+                        $container.html(infoForPain);
+                    });
                 });
-            });
-            let infoForPain = builder.makeInfoPane(searchResultAttractions);
-            $container.html(infoForPain);
         });
-    });
 }
-
-module.exports = {loadInfoByTime, loadInfoByClick};
+module.exports = { loadInfoByTime, loadInfoByClick };
