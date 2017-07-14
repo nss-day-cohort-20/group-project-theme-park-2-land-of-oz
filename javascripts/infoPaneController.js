125a0895bb407bba4956a1dfcd4aea0a499e37a3
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-07-14 11:35:56 -0500

    Park hours for undefined time elements

--- javascripts/infoPaneController.js
+++ javascripts/infoPaneController.js
@@ -6,46 +6,50 @@ let search = require('./search.js');
 let builder = require('./templateBuilder.js');
 let $container = $('#infoPaneText');
 
-function loadInfoByTime(){
+function loadInfoByTime() {
     accordianFactory.getAttractions()
-    .then((attractions)=>{
-        let searchResultAttractions = search.timeMatcher(attractions);
-        accordianFactory.getAttractionTypes()
-        .then((attractionTypes)=>{
-            searchResultAttractions.forEach((attraction)=>{
-                attractionTypes.forEach((type)=>{
-                    // console.log("attractiontype", attraction.type_id);
-                    if(type.id === attraction.type_id){
-                        attraction.type = type.name;
-                    }
+        .then((attractions) => {
+            let searchResultAttractions = search.timeMatcher(attractions);
+            accordianFactory.getAttractionTypes()
+                .then((attractionTypes) => {
+                    searchResultAttractions.forEach((attraction) => {
+                        attractionTypes.forEach((type) => {
+                            if (type.id === attraction.type_id) {
+                                attraction.type = type.name;
+                            }
+                        });
+                    });
+                    searchResultAttractions.forEach((attraction) => {
+                        if (attraction.times === false) {
+                            attraction.times = `Open 9:00 AM to 10:00 PM`;
+                        }
+                    });
                 });
-            });
-            console.log("attractions", searchResultAttractions);
-            let infoForPain = builder.makeInfoPane(searchResultAttractions);
-            console.log("info", infoForPain);
-            $container.html(infoForPain);
-        });
-    });
-}
-
-function loadInfoByClick(whichArea){
-    accordianFactory.getAttractions()
-    .then((attractions)=>{
-        let searchResultAttractions = search.areaMatcher(attractions, whichArea);
-        accordianFactory.getAttractionTypes()
-        .then((attractionTypes)=>{
-            searchResultAttractions.forEach((attraction)=>{
-                attractionTypes.forEach((type)=>{
-                    // console.log("attractiontype", attraction.type_id);
-                    if(type.id === attraction.type_id){
-                        attraction.type = type.name;
-                    }
-                });
-            });
             let infoForPain = builder.makeInfoPane(searchResultAttractions);
             $container.html(infoForPain);
         });
-    });
 }
-
-module.exports = {loadInfoByTime, loadInfoByClick};
+function loadInfoByClick(whichArea) {
+    accordianFactory.getAttractions()
+        .then((attractions) => {
+            let searchResultAttractions = search.areaMatcher(attractions, whichArea);
+            accordianFactory.getAttractionTypes()
+                .then((attractionTypes) => {
+                    searchResultAttractions.forEach((attraction) => {
+                        attractionTypes.forEach((type) => {
+                            if (type.id === attraction.type_id) {
+                                attraction.type = type.name;
+                            }
+                            searchResultAttractions.forEach((attraction) => {
+                                if (attraction.times === undefined) {
+                                    attraction.times = {"operatingHours" : `Open 9:00 AM to 10:00 PM`};
+                                }
+                            });
+                        });
+                        let infoForPain = builder.makeInfoPane(searchResultAttractions);
+                        $container.html(infoForPain);
+                    });
+                });
+        });
+}
+module.exports = { loadInfoByTime, loadInfoByClick };
